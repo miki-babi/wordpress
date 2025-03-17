b722b958b8c533575f2d3d6d06bbc7fce63534f9
miki-babi <mikiyasshiferaw@gmail.com>
2025-03-18 01:01:11 +0300

    config

--- wp-config.php
+++ wp-config.php
@@ -20,19 +20,19 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress' );
+define( 'DB_NAME', 'axumcoff_word' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'axumcoff_word' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'hPr(L8d-rmKK' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
 
 /** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8mb4' );
+define( 'DB_CHARSET', 'utf8mb3_unicode_ci' );
 
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
